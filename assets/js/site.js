9a565fc94af39d51379f03e342a9e82c6bf19dd8
Adrian Alcides Mendez Vargas <adrian_mendez@ucne.edu.do>
2021-10-05 21:00:23 -0400

    initial

---
+++ assets/js/site.js
@@ -0,0 +1,13 @@
+
+
+// var tables = document.querySelectorAll('table#table16');
+// for (const table of tables) {
+// 	let link = table.querySelector('a');
+// 	let uri = link.getAttribute('href');
+	
+// 	table.addEventListener('click', () => {
+// 		window.open(uri);
+// 	})
+
+// 	console.log(table);
+// }
